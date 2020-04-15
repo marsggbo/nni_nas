f4f6a795ba77d77ab72144d05f7cf03713a7b7de
marsggbo <1435679023@qq.com>
2020-04-15 14:48:48 +0800

    add mnist and cifar10 dataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -2,7 +2,32 @@
 # Licensed under the MIT license.
 
 import torch
+from torchvision.datasets import CIFAR10 as _CIFAR10
+from torchvision.datasets import MNIST as _MNIST
+
 from .build import DATASET_REGISTRY
+from .transforms import build_transforms
+
+__all__ = [
+    'MNIST',
+    'CIFAR10',
+    'FakeData',
+    'fakedata'
+]
+
+@DATASET_REGISTRY.register()
+def MNIST(cfg):
+    root = cfg.dataset.dir
+    is_train = cfg.dataset.is_train
+    transform = build_transforms(cfg)
+    return _MNIST(root=root, train=is_train, transform=transform.transform, download=True)
+
+@DATASET_REGISTRY.register()
+def CIFAR10(cfg):
+    root = cfg.dataset.dir
+    is_train = cfg.dataset.is_train
+    transform = build_transforms(cfg)
+    return _CIFAR10(root=root, train=is_train, transform=transform.transform, download=True)
 
 class FakeData(torch.utils.data.Dataset):
     def __init__(self, size=32, classes=10):
